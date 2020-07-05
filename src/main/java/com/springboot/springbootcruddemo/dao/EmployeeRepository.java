ea50a9c02118caeac984df1850a47c8ea958fa3c
Mateonidas <teus.polec@gmail.com>
2020-07-05 12:46:52 +0200

    Added support for Spring Boot Data REST.

---
+++ src/main/java/com/springboot/springbootcruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.springbootcruddemo.dao;
+
+import com.springboot.springbootcruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {}
